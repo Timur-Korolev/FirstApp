45b534ca36ae514df2474c711ab71fdbc0eefe5c
Timur Korolev <timurkorolev89@gmail.com>
2019-09-17 23:13:53 +0300

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by admin on 9/17/19.
+//  Copyright © 2019 Timur Korolev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
